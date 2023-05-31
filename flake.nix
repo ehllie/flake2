4a6fa8b314dd28b6eed026f4fe7ea5f901e7a52f
Elizabeth Paź <me@ehllie.xyz>
2023-05-31 15:56:12 +0700

    Build for all systems

--- flake.nix
+++ flake.nix
@@ -1,7 +1,16 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs, ... }: {
-    packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.hello;
-  };
+  outputs = { nixpkgs, ... }:
+    let
+      eachSystem = pkgBuild:
+        builtins.listToAttrs (builtins.map
+          (system: { name = system; value = pkgBuild system; })
+          [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ]);
+    in
+    {
+      packages = eachSystem (system: {
+        default = nixpkgs.legacyPackages.${system}.hello;
+      });
+    };
 }
